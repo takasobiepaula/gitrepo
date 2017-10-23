b1e41f8599391bdc7dffde6195c5b971e9e5e35c
takasobiepaula <paulinasuter0203@gmail.com>
2017-10-23 12:35:47 +0200

    tabele

---
+++ cpp/tabele.cpp
@@ -0,0 +1,31 @@
+/*
+ * tabele.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+void sumuj(int tab[], int ile){
+    int i=0;
+    int suma=0;
+for (i = 0; i < ile; i++) {
+       cout << "Podaj liczbę: ";
+       cin >> tab[i];
+}
+int main(int argc, char **argv)
+{
+    int rozmiar=0;
+    cout<<"Ile liczb podasz?";
+    cin>>rozmiar;
+    
+    int liczby[rozmiar]
+    
+    pobierzLiczby(liczby, rozmair)
+    sumuj(liczby,rozmiar);
+       
+}
+return 0;
+}
+	
+
+
---
+++ cpp/zwieksz.cpp
@@ -0,0 +1,26 @@
+/*
+ * zwieksz.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+int zwieksz((int liczba, int krok)
+{
+	;iczba=liczba+krok;
+    krok=3*krok;
+    return krok
+
+void zwieksz2(int &a, int &b)
+ a=a+b;
+ b=3*b;
+}
+int liczba, krok;
+cout << "Poaj liczbe i krok";
+cin>>liczba>>krok;
+
+cout<<"liczba i krok:"<<liczba<<endl;
+
+	return 0;
+}
+
