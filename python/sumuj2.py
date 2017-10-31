baa2a7232d4a65b379408d1375c7b375970f994d
takasobiepaula <paulinasuter0203@gmail.com>
2017-10-31 10:35:56 +0100

    sumuj2

---
+++ python/sumuj2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    suma = 0
+    ile = 0
+    while True:
+        liczba = int(input('Podaj liczbę: '))
+        suma = suma + liczba
+        ile = ile + 1
+        if suma > 100:
+            break
+    print('Wprowadzono', ile, 'liczb')
+    print('Suma:' , suma)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
