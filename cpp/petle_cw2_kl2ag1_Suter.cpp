f8f67ad51f6891dc7661cb63b463696a4a391b17
takasobiepaula <paulinasuter0203@gmail.com>
2017-11-05 18:25:13 +0100

    petle

---
+++ cpp/petle_cw2_kl2ag1_Suter.cpp
@@ -0,0 +1,33 @@
+/*
+ * petle_cw2_kl2ag1_Suter.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+	int a, b, c , obwod;
+	a = b = c = obwod = 0;
+	float p = 0;
+	cout << "Podaj boki: " << endl;
+	cin >> a >> b >> c;
+	
+	while(1)
+{   if ((a + b > c) && (a + c > b) && (b + c > a))
+	{
+		obwod = a + b + c;
+		p = 0.5 * obwod;
+		cout << "Pol: " << sqrt(p *( p-a) * (p-b) * (p-c))<< endl;
+		break;
+	}
+	else
+	{
+		cout << " Podaj boki: " << endl;
+		cin >> a >> b >> c;
+	}
+}
+	
+	return 0;
+}
+
